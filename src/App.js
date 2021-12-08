d4d69affec38b105933c8ac5cd819b70db3beccc
Milan <milanvpage@gmail.com>
2021-12-08 00:30:20 -0800

    add state to App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,14 @@ import React from 'react';
 import NavBar from './presentational/NavBar';
 
 export default class App extends React.Component {
-  
+  // set state that determines what category we're on
+  // we have 3 categories so for now we're goign to set it uo like a string for what category we're on
+  state = {
+    category: "commercial" //defualt commercial for now
+  }
+
+  // need setState() if we're using state, otherwise there's no point in having state
+  // create function to change state
   render(){
     return (
       <Router>
