58ba967158a0a1959cf238165d4763f08cf31e04
Milan <milanvpage@gmail.com>
2021-12-08 02:48:38 -0800

    add goback function button to theater and concert Components too

--- src/concert/ConcertContainer.js
+++ src/concert/ConcertContainer.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
 export default function ConcertContainer() {
+    
+    function handleClick(){
+        console.log("click")
+        props.routerInfo.history.goBack()
+    }
+    
     return (
         <div>
+            <button onClick={handleCLick}>Back</button>
             <h1>Concert Choreography</h1>
             {/* map through data here once we have our backend and FETCH request set up */}
 
--- src/theater/TheaterContainer.js
+++ src/theater/TheaterContainer.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
 export default function TheaterContainer() {
+    
+    function handleClick(){
+        console.log("click")
+        props.routerInfo.history.goBack()
+    }
+    
     return (
         <div>
+            <button onClick={handleCLick}>Back</button>
             <h1>Theater Choreography</h1>
             {/* map through data here once we have our backend and FETCH request set up */}
         </div>
