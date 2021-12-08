d93763148264f5f3122b06c3cd2681861a2b132d
Milan <milanvpage@gmail.com>
2021-12-08 02:34:39 -0800

    added routes inside my Categories Component so my routes will dynamically change and I set them to associate with each specific container that matches the route plus passed throguh routerProps so later we can access the gobck and history methods

--- src/Categories.js
+++ src/Categories.js
@@ -1,14 +1,28 @@
 import React from 'react'
+import CommercialContainer from './commercial/CommercialContainer'
+import ConcertContainer from './concert/ConcertContainer'
+import TheaterContainer from './theater/TheaterContainer'
+import {
+    Switch,
+    Route
+} from 'react-router-dom'
 
-
-export default function Categories({category}) {
+export default function Categories({category, commercial, concert, theater}) {
     //Once we have those Components set up - We can then Create another function in my Pages.js that we can use to determine which page I’m rendering
     
     
     
     return (
         <div>
-            
+            <Route 
+            exact path="/commercial" 
+            render= {routerProps => <CommercialContainer commercial={commercial} routerInfo={routerProps}/>}/>
+            <Route 
+            exact path="/concert" 
+            render= {routerProps => <CommercialContainer commercial={concert} routerInfo={routerProps}/>}/>
+            <Route 
+            exact path="/theater" 
+            render= {routerProps => <CommercialContainer commercial={theater} routerInfo={routerProps}/>}/>
         </div>
     )
 }
