4a4b0bdda91cbae2a23f1e905c2877dc995a5c8e
Milan <milanvpage@gmail.com>
2021-12-08 01:23:00 -0800

    add NavLinks to NavBar and pass props into Categories

--- src/Categories.js
+++ src/Categories.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
-export default function Categories() {
+
+export default function Categories({category}) {
+    //Once we have those Components set up - We can then Create another function in my Pages.js that we can use to determine which page I’m rendering
+    
+    
+    
     return (
         <div>
-            
+            <NavLink to="/commercial"
+            className={isSelected}
+            >StarShips</NavLink>
+            <NavLink to="/concert"
+            className={isSelected}
+            >Planets</NavLink>
+            <NavLink to="/theater"
+            className={isSelected}
+            >People</NavLink>
         </div>
     )
 }
