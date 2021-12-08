45f993f6e3fd4511b3a9fb49206f46550a3bc48d
Milan <milanvpage@gmail.com>
2021-12-08 02:06:35 -0800

    update state inside my App.js so I can hold all the data I'll need for my categories, created a commercial, concert, and theater keys that are set to an empty array, which is where we're store our data when we render each one on the page

--- src/App.js
+++ src/App.js
@@ -12,7 +12,10 @@ export default class App extends React.Component {
   // set state that determines what category we're on
   // we have 3 categories so for now we're goign to set it uo like a string for what category we're on
   state = {
-    category: "commercial" //defualt commercial for now
+    category: "commercial", //defualt commercial for now
+    commercial: [],
+    concert: [],
+    theater: []
   }
 
   // need setState() if we're using state, otherwise there's no point in having state
