50ca5c51b763b39f8fc20f5c48087917a21e3858
Milan <milanvpage@gmail.com>
2021-12-27 14:44:13 -0800

    added a bunch of notes inside reducer.js

--- src/components/pages/Pages.js
+++ src/components/pages/Pages.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Switch, Route } from 'react-router'
 import CommercialContainer from '../commercial/CommercialContainer'
+import ConcertContainer from '../concert/ConcertContainer'
 import TheaterContainer from '../theater/TheaterContainer'
 
 export default function Pages() {
@@ -9,6 +10,7 @@ export default function Pages() {
             <Switch>
                 <Route path="/commercial" component={routerProps => <CommercialContainer routerProps={routerProps} />}></Route>
                 <Route path="/theater" component={routerProps => <TheaterContainer routerProps={routerProps} />}></Route>
+                <Route path="/concert" component={routerProps => <ConcertContainer routerProps={routerProps} />}></Route>
             </Switch>
         </div>
     )
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -18,3 +18,17 @@ function reducer(state = {
 }
 
 export default reducer
+
+// takes two args state and action 
+// usually the action wold be a JS object
+// usually use SWITCH statement by convention 
+// look at our action type
+// default returns state 
+// return state or a new state depedning on what we have with our action 
+// can assign default/ initial state - state = {}
+// need to export to have access to it inside our store/ createStore function/method
+// doesn't matter what you name it when it's a defualt export
+// doesn't work that way for other exports - kinda like an object (key/value pairs) 
+// example our store dunction doesn;t have  name but we can still export default it
+// should be good to go with our store here
+//
