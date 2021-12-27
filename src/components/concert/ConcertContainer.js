afe167de7b32799ab2a7716308e43fc218499d4c
Milan <milanvpage@gmail.com>
2021-12-27 15:49:42 -0800

    set up my mapDiapatchToProps and passed it in my dipatch and then I pass my setConcert function to my dipatch, the function that I'm grabbing from my ConcertAction, my fetch request, - I can pass dipatch a function like this becasue I have set up thunk inside my store - I then have to pass my function inside my mapDispatchToProps dispatchSetConcert to my componentDidMount function becasuen I haveto mount it onto the page and dispatchSetConcert is where I'm passing my action to my dispatch through a function

--- src/components/concert/ConcertContainer.js
+++ src/components/concert/ConcertContainer.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { setConcert } from './../../redux/ConcertAction'
+
 
 class ConcertContainer extends Component {
     
+    componentDidMount(){
+        // where we fetch our data
+    this.props.dispatchSetConcert()
+    // the function I created inside my mapDipatchToProps
+    // I'm calling it here to mount it onto my page
+    }
     
     
     render() {
@@ -20,6 +29,13 @@ function mapStateToProps(stateFromStore){
     return{
         concert: stateFromStore.concert
     }
+}
+
+function mapDispatchToProps(dispatch){
+    return{
+        dispatchSetConcert: () => dispatch(setConcert())
+    } // we're importing a function from our ConcertAction and callingit inside dispatch
+    // we have thunk so that's why we cna pass dipatch a function 
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(ConcertContainer)
