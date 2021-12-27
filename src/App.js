e38ea86566704be92492a07cf5c9c904cd1b6195
Milan <milanvpage@gmail.com>
2021-12-27 15:27:27 -0800

    add a NavLink for Concert nside App.js so I can dispaly my Concert Link on the page

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ export default class App extends Component {
         <NavLink to ="/">Home</NavLink><br/>
         <NavLink to="/commercial">Commercial</NavLink>
         <NavLink to="/theater">Theater</NavLink>
+        <NavLink to="/concert">Concert</NavLink>
         <br/>
         <Pages />
       </div>
