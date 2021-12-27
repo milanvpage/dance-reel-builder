bcb4552f7db57544b27b8c2cd9dd1de64b4bee32
Milan <milanvpage@gmail.com>
2021-12-27 15:25:56 -0800

    set up ConcertAction file that fetchs all the data for concert dance from my backend by using the thunk middlware to dipatch an asynchronous function

---
+++ src/redux/ConcertAction.js
@@ -0,0 +1,20 @@
+// BEST PRATICE with th SET_CONCERT variable
+//  export const setConcert = (concert) => ({type SET_CONCERT, payload: concert})
+
+export const setConcert = () => {
+    
+    return (dispatch) => {
+        //fetch goes here
+        fetch("http://localhost:3000/concert")
+        .then(r => r.json())
+        .then(concerts => dispatch({type: "SET_CONCERT", payload: concerts}))
+    }
+    // {type: SET_CONCERT", payload: concert}
+}
+
+
+// (concert) is our array directly coming from our backend
+
+// add concert
+// edit concert
+ // delete concert
