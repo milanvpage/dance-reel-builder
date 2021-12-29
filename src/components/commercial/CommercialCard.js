8f3622d793b57ca226db70b5756c350c9a4f0bd5
Milan <milanvpage@gmail.com>
2021-12-29 13:11:24 -0800

    import in my action creator deleteCommercial from my CommercialAction.js so Ic an have access to it and call on it when I click my deletebutton - also need to import n connect so i can have access to my dipatch to merge my props and dispatch my action/ connect my react side to my redux, or else I won't be able to use my action

--- src/components/commercial/CommercialCard.js
+++ src/components/commercial/CommercialCard.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { deleteCommercial } from '../../redux/CommercialAction'
+import { connect } from 'react-redux' // need access to connect in order to dispatch
 
-export default function CommercialCard({name}) {
+function CommercialCard({name}) {
+
+    function handleDelete(){
+
+    }
     return (
         <div>
-            <li>{commercial.name}</li>
+            <li>{name}</li><button onClick={handleDelete}>X</button>
         </div>
     )
 }
+
+export default connect(null, {})(CommercialCard)
