74043737d0074e566b56a6269bc80a3142e421b7
Milan <milanvpage@gmail.com>
2021-12-29 09:37:08 -0800

    create handleSUbmit function to preventDefault so I can handle my http request with fetch instead of HTML, I don't want html to send a GET request - callingthis function inside my onSubmit in my form

--- src/components/commercial/CommercialForm.js
+++ src/components/commercial/CommercialForm.js
@@ -15,9 +15,16 @@ export default class CommercialForm extends Component {
         )
     } // calling an arrow function so we have the context of this from where we've defined it rather than where we're calling it(the scope from this function to pass "this" from this scope)
 
+    handleSubmit = () => {
+        e.preventDefault() // bcause I want to handle my http request with fetch, I don't want them handled bby html SO I'm preventing html from trying to send a GET request
+        // this is where we want to tie in our action, so we want to:
+        // dispatch addCommercial
+        // and clear out my state
+    }
+    // always need to prevent default with handleSubit
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <label htmlFor="commercial-name-input">Name:</label>
                 {/* for label we often want to give it a for element - react uses htmlFor prop which will translate to that for property and label */}
                 {/* SO what we're goignto do is add an id to my input field and make it unique so nothing else in my page has it */}
