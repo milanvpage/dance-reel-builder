40aed1b04d99494f3bd6fedda20108bd94c9ed9d
Milan <milanvpage@gmail.com>
2021-12-29 15:19:21 -0800

    finsihed the handleDelete function that we're calling when someone clicks on the X button inside our list items - passing in theid of the commercial and then taking that commercial and passing it ot the deleteCommercial function we created inside our CommercialAction - the commercial should then get deleted becasue of the fetch request and the action we are calling

--- src/components/commercial/CommercialCard.js
+++ src/components/commercial/CommercialCard.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { deleteCommercial } from '../../redux/CommercialAction'
 import { connect } from 'react-redux' // need access to connect in order to dispatch
 
-function CommercialCard({name}) {
+function CommercialCard({ id, name, deleteCommercial }) {
 
     function handleDelete(){
-
+        deleteCommercial(id) // this is actual  "dispatchDeleteComercial" the prop we created that's equal to a function that takes dispatch and dispatch(deleteCommercial(commercial))
     }
     return (
         <div>
@@ -13,5 +13,13 @@ function CommercialCard({name}) {
         </div>
     )
 }
-
-export default connect(null, {})(CommercialCard)
+// function mapDispatchToProps(dispatch){
+//     return {
+//         dispatchDeleteComercial: (commercial) => dispatch(deleteCommercial(commercial))
+//     }
+// }
+export default connect(null, { deleteCommercial })(CommercialCard)
+ // there's a nicer way to write our mapDispatchToProps WHERE we can just pass in an object rather than a function and it will do the same behavior as "function mapDispatchToProps(dispatch){}"
+ // IT basically takes our default behavior and just mocks it up so we have to do less of that code
+ // WHEN we add deleteCommercial to this {} object here it will do that default behavior and set up a prop of delteCommercial (dispatchDeleteCommercial) - WHICH will be equal to a function that will dispatch the return value of deleteCommercial
+ // AND it will also know like whatever we pass as an arg to this object it'll take in - like how we're passing a specific commercial to the deleteCommercial(commercial) function - we can still pass in that ID - it'll just do it for us
