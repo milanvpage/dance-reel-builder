b1532f47ce6c28857e848758fc6b94c3424571a9
Milan <milanvpage@gmail.com>
2021-12-29 09:40:34 -0800

    added setState({name: }) so I can clear my state when I hit my submit button - but I also first needed to add a value inside my input equal to my current state so i have somehting to clear when I call setState

--- src/components/commercial/CommercialForm.js
+++ src/components/commercial/CommercialForm.js
@@ -20,6 +20,7 @@ export default class CommercialForm extends Component {
         // this is where we want to tie in our action, so we want to:
         // dispatch addCommercial
         // and clear out my state
+        this.setState({name: ""}) // this will clear our name in the inout field so when I hit submit the input clears
     }
     // always need to prevent default with handleSubit
     render() {
@@ -29,7 +30,7 @@ export default class CommercialForm extends Component {
                 {/* for label we often want to give it a for element - react uses htmlFor prop which will translate to that for property and label */}
                 {/* SO what we're goignto do is add an id to my input field and make it unique so nothing else in my page has it */}
                 {/* HTML when we click on the name on our page it highlights the input field - f I don't have the htmlFor and nthe id number we lose that functionality on our page   */}
-                <input id="commercial-name-input" onChange={this.handleChange} type="text" /> 
+                <input id="commercial-name-input" onChange={this.handleChange} value={this.state.name} type="text" /> 
                 {/* here my event handleer is going to call handleChange everytime there's an onclick and pass in the value of the function here, so it will no longer be associated to "this" by the time it's called, unless we mkae it an arrow function - which it will then take it's context from where it's defined */}
                 {/* what we're doing here is - whenever we define an event listener and we're passing something in it's going to take the return vslue of "this.handleChange" which is our function and then it will save that return value to call anytime it's changed */}
                 {/* WHen this input fiel dis added to the DOM and they  add that event listner it evaluates the onChange and it looks at this.handleSubmit a function "this.handleSubmit #=> f(){} - and then this is what it has, this function "f(){}" to be able to call everytime there is a change - it no longer looks at this.handleSubmit, becasue we're not calling  this.handleSubmit() like this everytime that it's changed, it's calling this function f(){}, it's no longer tied to  */}
