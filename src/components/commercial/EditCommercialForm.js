94e004189b7cf3a6692f9e3e907aa571b32f2364
Milan <milanvpage@gmail.com>
2021-12-30 12:10:13 -0800

    trying to figure out how to connect my id in my state from my store array to my id in my route, the id I can gain access to my using my routerProps and going through match.params.id

--- src/components/commercial/EditCommercialForm.js
+++ src/components/commercial/EditCommercialForm.js
@@ -2,16 +2,30 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class EditCommercialForm extends Component {
-    // set up intial state
-    state = {
+    
+    constructor(props){
+        super(props)
+        this.state = {
+            name: props.commercial.find(c => {
+                c.id === parseInt(props.routerProps.match.params.id) // need to parseInt this becasue what's coming from my URL (from my match from my router params remeber it was a string) whereas my id from in here should be an integer
+                // want to look at my routerProps so I can get to match and grab that params that shows us what id we have, that way I can compare the id to the id that matches my array I get from my store of my state of commercials
+            }).name
+            // I wnat to find the person in this array that matches the id from my Router 
+            // how I would determine my initial state would be based on what's in my store and coming from my PROPS
+        }
+    }
 
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
     }
 
     render() {
         console.log(this.props)
         return (
             <form>
-                <input type="text"/>
+                <input value={this.state.name} onChange={this.handleChange} name="name" type="text"/>
                 <input type="submit"/>
             {/* from here what I would like to do is look at my routerProps */}
             {/* when I look at my router props I can determine from match and see which id we're talking about when I go to the EditFormRoute */}
