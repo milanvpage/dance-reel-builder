5797836f9d947d410c7b3cfd49db30ab672aa846
Milan <milanvpage@gmail.com>
2021-12-30 13:23:57 -0800

    set up my handleCHange function in my theater and concert forms so when I click on my inout I can change my state to the value I type in

--- src/components/theater/TheaterForm.js
+++ src/components/theater/TheaterForm.js
@@ -6,6 +6,11 @@ export default class TheaterForm extends Component {
         name: ""
     }
     
+    handleChange = (e) => {
+        // this.setState({[e.target.name]: e.target.value} // for if we have more than one input field // then manually set all our input fields to have a name so that we have one handleChange
+        this.setState({name: e.target.value})
+    } // calling an arrow function so we have the context of this from where we've defined it rather than where we're calling it(the scope from this function to pass "this" from this scope)
+    
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
