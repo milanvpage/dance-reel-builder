64150e8d1f52f6b53e899f7792c0566962f18bf7
Milan <milanvpage@gmail.com>
2021-12-30 13:15:38 -0800

    created a file for my ConcertForm  made it a class component

---
+++ src/components/concert/ConcertForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ConcertForm extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
