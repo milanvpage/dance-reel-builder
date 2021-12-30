28d2b69319f65644c076d59b2493f21eabaa1e64
Milan <milanvpage@gmail.com>
2021-12-30 11:50:39 -0800

    added input fields in my form for EditCommercialForm and logged my props so I can see my routerProps and check out if match show my id when I go to my route

--- src/components/commercial/EditCommercialForm.js
+++ src/components/commercial/EditCommercialForm.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react'
 
 export default class EditCommercialForm extends Component {
     render() {
+        console.log(this.props)
         return (
-            <div>
-                
-            </div>
+            <form>
+                <input type="text"/>
+                <input type="submit"/>
+            {/* from here what I would like to do is look at my routerProps */}
+            {/* when I look at my router props I can determine from match and see which id we're talking about when I go to the EditFormRoute */}
+            {/* need to connect to my store because I need access to all of the commercials in my store */}
+            </form>
         )
     }
 }
