7f4cbbc4793cce0a1655c3987fc85657eacf4394
Milan <milanvpage@gmail.com>
2021-12-30 14:58:43 -0800

    created delete button inside ConcertCard

--- src/components/concert/ConcertCard.js
+++ src/components/concert/ConcertCard.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 export default function ConcertCard() {
+    
+
+    
     return (
         <div>
-            
+            <li>{props.name}</li>
+            <button onClick={handleDelete}>X</button>
         </div>
     )
 }
